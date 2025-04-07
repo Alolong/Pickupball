d23f7ca9fa12d21a6da6a266f1b7dc0e09519573
Alolong <ddten123@gmail.com>
2025-04-07 17:53:25 +0900

    0407-05:53

--- Rand/PouchBall.cpp
+++ Rand/PouchBall.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 void main()
 {
-	
-	int maxcount = 0;
+int i = 53;
+int nRandNum = 0;
+bool bSame = false;
+nRandNum = rand() % 52 + 1;
+int nNum[53] = { 0 };
+nNum[i] = nRandNum;
 
-	for (int j = 1; j < 53; j++)
+for (int j = 1; j < i; j++)
+
+	
 	{
-
-	cout << "You Picked up the Ball number" << j << endl;
+		if (j == nRandNum)
+		{
+			bSame = true;
+			break;
+				
+		}
+		else (bSame)
+		{
+			i--;
+			continue;
+		}
+		cout << "You Picked up the Ball number" << j << endl;
 	}
+	//같으면 다시 반복
+	
 	
 
-
-	
 
 }
+
+
+//섞은 후 차례대로 뽑을지,
+//
+// 현실에 있는 걸 똑같이 만든다는 생각을 가질 것
+// 
+// 
+// 아니면 랜덤으로 뽑을지
+
+
 

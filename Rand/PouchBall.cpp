21e794a09db511af6846d7683a4c64870bd2db38
Alolong <ddten123@gmail.com>
2025-04-07 09:27:19 +0900

    Initial commit

---
+++ Rand/PouchBall.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	
+	int maxcount = 0;
+
+	for (int j = 1; j < 53; j++)
+	{
+
+	cout << "You Picked up the Ball number" << j << endl;
+	}
+	
+
+
+	
+
+}
+
